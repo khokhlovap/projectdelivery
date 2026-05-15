95917cbb1d1579466f6e64ef33ab044966c78d10
khokhlovap <polina_2304@bk.ru>
2026-05-15 11:37:29 +0300

    Email настройки вынесены в переменные окружения

--- deliverysite/deliverysite/settings.py
+++ deliverysite/deliverysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,10 +152,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'avn-biz@mail.ru'
-EMAIL_HOST_PASSWORD = '...'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_USE_SSL = False 
-
+EMAIL_USE_SSL = False
 # Для разработки (письма будут выводиться в консоль)
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
